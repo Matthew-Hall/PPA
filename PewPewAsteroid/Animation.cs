d1461130365b7d1c40b6c5d73a9439a610a527e6
Matthew Hall <43222050+MattAHK@users.noreply.github.com>
2019-01-28 14:25:57 -0500

    6.1.0

---
+++ PewPewAsteroid/Animation.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SpaceShooter
+{
+    class Animation
+    {
+        Texture2D spriteSheet;
+        public float scale;
+        int elapsedTime;
+        int frameTime;
+        int frameCount;
+        int currentFrame;
+        Color color;
+        Rectangle sourceRect = new Rectangle();
+        Rectangle destinationRect = new Rectangle();
+        public int FrameWidth;
+        public int FrameHeight;
+        public bool Active;
+        public bool Looping;
+        public Vector2 Position;
+
+        public void Initialize(Texture2D texture, Vector2 position, int frameWidth, int frameHeight, int frameCount, int frametime, Color color, float scale, bool looping)
+        {
+            this.color = color;
+            this.FrameWidth = frameWidth;
+            this.FrameHeight = frameHeight;
+            this.frameCount = frameCount;
+            this.frameTime = frametime;
+            this.scale = scale;
+
+            Looping = looping;
+            Position = position;
+            spriteSheet = texture;
+
+            elapsedTime = 0;
+            currentFrame = 0;
+
+            // Set the Animation to active by default
+            Active = true;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            // Do not update the game if we are not active
+            if (Active == false) return;
+            // Update the elapsed time
+            elapsedTime += (int)gameTime.ElapsedGameTime.TotalMilliseconds;
+            // If the elapsed time is larger than the frame time
+            // we need to switch frames
+            if (elapsedTime > frameTime)
+            {
+                // Move to the next frame
+                currentFrame++;
+
+                if (currentFrame == frameCount)
+                {
+                    currentFrame = 0;
+                    if (Looping == false)
+                        Active = false;
+                }
+
+                elapsedTime = 0;
+            }
+            // Grab the correct frame in the image strip by multiplying the currentFrame index by the Frame width
+            sourceRect = new Rectangle(currentFrame * FrameWidth, 0, FrameWidth, FrameHeight);
+            // Grab the correct frame in the image strip by multiplying the currentFrame index by the frame width
+            destinationRect = new Rectangle((int)Position.X - (int)(FrameWidth * scale) / 2,
+            (int)Position.Y - (int)(FrameHeight * scale) / 2,
+            (int)(FrameWidth * scale),
+            (int)(FrameHeight * scale));
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            if (Active)
+            {
+                spriteBatch.Draw(spriteSheet, destinationRect, sourceRect, color);
+            }
+        }
+    }
+}
